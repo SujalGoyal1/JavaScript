4b5fc732e66ecd32e2ec51115f1942a546a5ef80
SujalGoyal12 <144935518+SujalGoyal1@users.noreply.github.com>
2025-08-26 18:28:22 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,25 @@
+"use scrict"  // treat all JS code as newer version
+
+//alert( 3 + 3)  // we are using nodejs, not browser
+
+console.log(3 + 3)   // code readability should be high
+
+console.log("Hitesh");
+
+let name = "sujal"
+let age = 17
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => find uniqueness
+
+
+// object
+
+console.log(typeof undefined);  // undefind
+console.log(typeof null); // object
